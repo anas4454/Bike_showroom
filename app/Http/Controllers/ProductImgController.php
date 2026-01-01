0a8ac93702d1e74d3a0676b59972ea9ab982bb67
Muhammad Anas <manasali493@gmail.com>
2026-01-01 19:24:36 +0500

    Initial commit

---
+++ app/Http/Controllers/ProductImgController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product_img;
+use App\Http\Requests\StoreProduct_imgRequest;
+use App\Http\Requests\UpdateProduct_imgRequest;
+
+class ProductImgController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProduct_imgRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product_img $product_img)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product_img $product_img)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProduct_imgRequest $request, Product_img $product_img)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product_img $product_img)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_01_01_132531_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("slug")->unique();
+            $table->integer("old_price");
+            $table->integer("new_price");
+            $table->longtext("description");
+            $table->string("excerpt");
+            $table->integer("category_id");
+            $table->string("brand");
+            $table->string("condition");
+            $table->string("model_name");
+            $table->string("bike_type");
+            $table->string("launch_year");
+            $table->integer("no_of_cylinder");
+            $table->integer("engine_cc");
+            $table->integer("max_meter");
+            $table->integer("max_torque");
+            $table->integer("no_of_gear");
+            $table->integer("tank_capacity");
+            $table->integer("milage");
+            $table->integer("weight");
+            $table->string("break_type");
+            $table->string("wheel_type");
+            $table->string("tyre_type");
+            $table->string("headlight");
+            $table->string("colors");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
